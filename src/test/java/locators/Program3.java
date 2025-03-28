3762919a062a0a68c9852a821d7b9fd40b21977a
nikitasontake <101354370+nikitasontake@users.noreply.github.com>
2025-03-28 11:12:36 +0530

    Test3

---
+++ src/test/java/locators/Program3.java
@@ -0,0 +1,7 @@
+package locators;
+
+public class Program3 {
+    public static void main(String[] args){
+        System.out.println("Program 3");
+    }
+}
---
+++ src/test/java/locators/TestPro1.java
@@ -0,0 +1,7 @@
+package locators;
+
+public class TestPro1 {
+    public static void main(String[] args){
+        System.out.println("Test Program 1 from Test1 branch");
+    }
+}
